f7a8e94414a1a1e8c349ab88e9979f9ca849cb8d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-31 08:30:59 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,14 +10,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns false"""
-        if path is None:
-            return True
         if excluded_paths is None or excluded_paths == []:
+            return True
+        if path is None or path not in excluded_paths:
             return True
         if path in excluded_paths:
             return False
-        if path not in excluded_paths:
-            return True
 
     def authorization_header(self, request=None) -> str:
         """return None"""
