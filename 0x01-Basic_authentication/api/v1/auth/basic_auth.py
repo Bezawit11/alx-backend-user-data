80e208568d37d3ae61ad642c55d681a87012d39f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-03 12:20:33 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of auth
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,15 @@ class BasicAuth(Auth):
             return None
         r = authorization_header.split(' ')[-1]
         return r
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """decodes base64"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            d = b64decode(base64_authorization_header.encode('utf-8'))
+            return d.decode('utf-8')
+        except BaseException:
+            return None
