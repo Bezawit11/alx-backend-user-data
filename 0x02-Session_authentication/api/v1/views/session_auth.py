70d81f113f784dd50e5b0d8ece811224a7b04a8f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-04 14:59:28 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -29,3 +29,13 @@ def ses_auth():
     response = jsonify(user.to_json())
     response.set_cookie(SESSION_NAME, session_id)
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """for deleting the Session ID contains in the request as cookie"""
+    from api.v1.app import auth
+    b = auth.destroy_session(request)
+    if not b:
+        abort(404)
+    return jsonify({}), 200
+
