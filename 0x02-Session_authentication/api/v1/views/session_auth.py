e54ae8a76fda594fbfda66eff1d2c7c43c4a0f26
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-04 14:34:34 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -20,16 +20,13 @@ def ses_auth():
     if not users:
         return jsonify({"error": "no user found for this email"}), 404
     for i in users:
-        if not i.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
+        if i.is_valid_password(password):
+            user = i
+    if not user:
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
-    user = users[0]
     session_id = auth.create_session(user.id)
-
     SESSION_NAME = getenv("SESSION_NAME")
-
     response = jsonify(user.to_json())
     response.set_cookie(SESSION_NAME, session_id)
-
     return response
-
