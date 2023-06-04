26decce226c175a3a849af0585f1e617126a29cb
Bezawit11 <socbeza13@gmail.com>
2023-06-04 12:14:05 +0000

    session class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
+from api.v1.auth.auth import Auth
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
+
+
+class SessionAuth(Auth):
+    pass
