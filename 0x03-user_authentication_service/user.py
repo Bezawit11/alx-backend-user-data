9112d90cceb05bf8714b7e8dcb6d88cc33ae9f74
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-07 09:06:35 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-"""
-user
-"""
-class
+"""users table"""
+
+
+class User(Base):
+    """users table sqlalchemy"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
