81bcd5f028a60f1af26f507a007c05bdd2acbcab
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-07 13:42:14 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
+"""hashpasswrd method"""
+
+
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """takes in a password string arguments and returns bytes"""
+    return bcrypt.hashpw(password, bcrypt.gensalt())
