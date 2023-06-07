261902f1afe58dcdfbe32ac90326575073d2d9de
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-07 10:36:20 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -36,3 +37,11 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, arbitrary: str) -> User:
+        """finds user from database"""
+        try:
+            a = self._session.query(User).filter(email=email).first()
+            return a
+        except NoResultFound:
+            return None
