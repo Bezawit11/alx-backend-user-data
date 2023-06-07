57bf11c05b2a56312d53b545aa762fc8cb265eaf
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-07 14:01:45 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@
 import bcrypt
 from user import User
 from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 def _hash_password(password: str) -> str:
     """takes in a password string arguments and returns bytes"""
