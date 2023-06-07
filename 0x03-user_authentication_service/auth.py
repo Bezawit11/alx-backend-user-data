aa55382d923670fbc95e8c3454a0dbdb9e5315f6
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-07 14:01:08 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,11 +23,10 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-        except:
-            raise ValueError(f'User {email} already exists')
-        pwd = _hash_password(password)
-        new_user = self._db.add_user(email, pwd)
-        return new_user
+            if user:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            pwd = _hash_password(password)
+            new_user = self._db.add_user(email, pwd)
+            return new_user
 
-        
-        
