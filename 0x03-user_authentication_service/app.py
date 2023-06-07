c94d3bab713222a7f9d8c1d5658db59a0fd1a983
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-07 14:08:12 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
+"""Basic Flask App"""
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+@aap.route('/')
+def index():
+    """jsonifies"""
+    return jsonify({"message": "Bienvenue"})
+    
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
