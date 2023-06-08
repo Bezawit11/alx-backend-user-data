8d93c1f70598e478b1c5c340dad27296b4b05724
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-08 09:18:11 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -22,7 +22,7 @@ def users():
         auth.register_user(email, pwd)
         return jsonify({"email": "<registered email>", "message": "user created"})
     except ValueError:
-        return jsonify({"message": "email already registered"})
+        return jsonify({"message": "email already registered"}), 400
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
