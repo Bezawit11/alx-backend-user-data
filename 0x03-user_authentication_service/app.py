08a7afa4570fdbf82ee12eeae51bab2d993e5e40
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-08 12:46:15 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 
 
 from auth import Auth
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 
 
 app = Flask(__name__)
@@ -42,6 +42,37 @@ def login() -> str:
         return res
     abort(401)
 
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """logs out a logged in user"""
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        redirect('/')
+    abort(403)
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """finds user based on the cookie session id"""
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """"resets the password of a user if the right credentials r provided"""
+    
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """update password of a user if valid"""
+    
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
