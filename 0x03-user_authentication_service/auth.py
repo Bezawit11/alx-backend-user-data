3f8abc3f0ca0db64574c61d3a9979540223ddbf1
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-08 11:50:11 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,7 +56,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             if user:
                 session_id = _generate_uuid()
-                self._db.update_user(user.id, session_id)
+                self._db.update_user(user.id, session_id=session_id)
                 return user.session_id
         except NoResultFound:
             return None
