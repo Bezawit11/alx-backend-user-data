0cc9411bb8cb66fe6aacfd967b1de60456f190af
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-08 10:00:19 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
 """API Routes for Authentication Service"""
 from auth import Auth
-from flask import (Flask,
-                   jsonify,
-                   request,
-                   abort,
-                   redirect)
+from flask import Flask, jsonify, request, abort
+
 
 app = Flask(__name__)
 AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
-def hello_world() -> str:
-    """ Base route for authentication service API """
-    msg = {"message": "Bienvenue"}
-    return jsonify(msg)
+def index() -> str:
+    """homepage to our app; imploys get request"""
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'])
-def register_user() -> str:
-    """Registers a new user if it does not exist before"""
+def users() -> str:
+    """Registers a new user if it doen't exist already"""
     try:
         email = request.form['email']
-        password = request.form['password']
-    except KeyError:
+        pwd = request.form['password']
+    except Exception:
         abort(400)
-
     try:
-        user = AUTH.register_user(email, password)
+        user = AUTH.register_user(email, pwd)
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
-    msg = {"email": email, "message": "user created"}
-    return jsonify(msg)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
