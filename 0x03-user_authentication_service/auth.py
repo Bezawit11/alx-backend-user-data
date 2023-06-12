89d6570f202f34bc0d592fd9e31763a1a3119dfe
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-12 10:14:25 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,7 +61,7 @@ class Auth:
         except NoResultFound:
             return None
     
-    def get_user_from_session_id(self, session_id: str) -> User
+    def get_user_from_session_id(self, session_id: str) -> User:
         """finds a user coressponding with the provided session id"""
         if session_id is None:
             return None
